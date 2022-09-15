7b2f0e3a21a673017496a7c33ea71a67c2b328b0
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-09-16 05:05:17 +1000

    update

--- src/Components/customHooks/useDarkSide.js
+++ src/Components/customHooks/useDarkSide.js
@@ -4,13 +4,13 @@ export default function useDarkSide() {
     const [theme, setTheme] = useState(localStorage.theme || "light")
     const colorTheme = theme === 'dark' ? 'light' : 'dark';
 
-    // useEffect(() => {
-    //     const root = window.document.documentElement;
-    //     root.classList.remove(colorTheme);
-    //     root.classList.add(theme);
+    useEffect(() => {
+        const root = window.document.documentElement;
+        root.classList.remove(colorTheme);
+        root.classList.add(theme);
 
-    //     localStorage.setItem('theme', theme);
-    // }, [theme, colorTheme])
+        localStorage.setItem('theme', theme);
+    }, [theme, colorTheme])
 
     return [colorTheme, setTheme];
 }
