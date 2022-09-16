669f26250344746ea68cdb89502e7d5e4e6bbac1
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-09-16 16:40:59 +1000

    update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Layout from './Components/global/Layout';
 import { NotificationContainer } from "react-notifications";
 import 'react-notifications/lib/notifications.css';
 import BlockchainProvider from "./context";
-import "./Assets/css/nucleo-icons.css"
 import "./Assets/css/app.css"
-import "./Assets/css/nucleo-svg.css"
-import "./Assets/css/index.css"
 import "./Assets/css/custom.css"
 import "./Assets/css/mycss.css"
 function App() {
